af20a019f17784d5424ec3df84cb615b7b24202a
ramakrishna <chimmani.ramakrishna@accenture.com>
2022-01-30 23:31:30 +0530

    stati folder and assets were created

--- Hello_World/Hello_World/urls.py
+++ Hello_World/Hello_World/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('calc.urls')),
+    path('travello', include('travello.urls')),
     path("admin/", admin.site.urls),
 ]
