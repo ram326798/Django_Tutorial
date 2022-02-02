a88b4bff4129a4f860faa3a3b98f5aaa8d8ed5d8
ramakrishna <chimmani.ramakrishna@accenture.com>
2022-02-02 11:32:17 +0530

    django tutorial

--- Hello_World/accounts/views.py
+++ Hello_World/accounts/views.py
@@ -1,3 +1,5 @@
+import re
+from django.contrib import messages
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User, auth
 # Create your views here.
@@ -12,9 +14,45 @@ def register(request):
         email = request.POST['email']
         password1 = request.POST['password1']
         password2 = request.POST['password2']
-        user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password1=password1, password2=password2)
-        user.save();
-        print("User created")
-        return redirect('/')
+        if password1 == password2:
+            if User.objects.filter(username=username).exists():
+                # print("Username already taken")
+                messages.info(request, 'Username Taken')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                # print("Email already taken")
+                messages.info(request, 'Email Taken')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(
+                    first_name=first_name, last_name=last_name, username=username, email=email, password=password1)
+                user.save()
+                # print("User created")
+                # messages.info(request, 'User Registered Successfully')
+                return redirect('login')
+        else:
+            # print("password is not matching ...")
+            messages.info(request, 'Password is not Matching')
+            return redirect('register')
     else:
         return render(request, 'register.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if(user is not None):
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Invalid Credentials')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
+
+def logout(reqest):
+    auth.logout(reqest)
+    return redirect('/')
